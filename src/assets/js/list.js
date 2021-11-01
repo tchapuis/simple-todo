65b08aecd56b20b2bb385c6174c19bfb36b148e5
Thomas Chapuis <chapuis.t@gmail.com>
2021-11-01 10:30:21 +0100

    Added eslint and fixed code style

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -1,29 +1,28 @@
 const generateItem = (id, content) => {
-    const newListElement = document.createElement('li');
-    newListElement.id = `item-${id}`
-    newListElement.innerHTML =
-        `<input type="checkbox" class="mark-item" id="mark-item-${id}" data-item-id="${id}">
+  const newListElement = document.createElement('li');
+  newListElement.id = `item-${id}`;
+  newListElement.innerHTML = `<input type="checkbox" class="mark-item" id="mark-item-${id}" data-item-id="${id}">
         <label for="mark-item-${id}">${content}</label>
         <button id="delete-item-${id}">Delete</button>`;
 
-    return newListElement;
-}
+  return newListElement;
+};
 
 const addItem = () => {
-    const itemsListEl = document.querySelector('#items-list');
-    const inputValue = document.querySelector('#add-item-input').value;
-    const listElementsCount = itemsListEl.childElementCount
+  const itemsListEl = document.querySelector('#items-list');
+  const inputValue = document.querySelector('#add-item-input').value;
+  const listElementsCount = itemsListEl.childElementCount;
 
-    itemsListEl.append(generateItem(listElementsCount + 1, inputValue));
-}
+  itemsListEl.append(generateItem(listElementsCount + 1, inputValue));
+};
 
 const checkItem = (e) => {
-    const itemId = e.currentTarget.dataset.itemId;
-    document.querySelector(`#item-${itemId} > label`).classList.add('line-through');
-}
+  const { itemId } = e.currentTarget.dataset;
+  document.querySelector(`#item-${itemId} > label`).classList.add('line-through');
+};
 
 document.querySelector('#add-item-button').addEventListener('click', addItem);
 
-document.querySelectorAll('.mark-item').forEach(checkbox => {
-    checkbox.addEventListener('click', checkItem);
-})
+document.querySelectorAll('.mark-item').forEach((checkbox) => {
+  checkbox.addEventListener('click', checkItem);
+});
