afcd8d3ab8a41e68fb102d97ad46f7d2e3f8b057
Laurie <l.queron@hotmail.fr>
2021-11-08 12:14:07 +0100

    Deleting an item is not working

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -1,12 +1,32 @@
+import { log } from 'console';
+
 const generateItem = (id, content) => {
   const newListElement = document.createElement('li');
   newListElement.id = `item-${id}`;
   newListElement.innerHTML = `<input type="checkbox" class="mark-item" id="mark-item-${id}" data-item-id="${id}">
         <label for="mark-item-${id}">${content}</label>
-        <button id="delete-item-${id}">Delete</button>`;
+        <button id="delete-item-${id}" class ="delete" data-item-id="${id}">Delete</button>`;
 
   return newListElement;
 };
+
+const deleteItem = (e) => {
+  const { itemId } = e.currentTarget.dataset;
+  document.querySelector(`li#item-${itemId}`).remove();
+};
+
+function deleteI() {
+  document.querySelectorAll('.delete').forEach((e) => {
+    e.addEventListener('click', deleteItem);
+  });
+}
+
+function newItem() {
+  const arrayDelete = document.querySelectorAll('.delete');
+  log(arrayDelete);
+}
+
+newItem();
 
 const addItem = () => {
   const itemsListEl = document.querySelector('#items-list');
@@ -14,12 +34,16 @@ const addItem = () => {
   const listElementsCount = itemsListEl.childElementCount;
 
   itemsListEl.append(generateItem(listElementsCount + 1, inputValue));
+  deleteI();
+  newItem();
 };
 
 const checkItem = (e) => {
   const { itemId } = e.currentTarget.dataset;
   document.querySelector(`#item-${itemId} > label`).classList.add('line-through');
 };
+
+deleteI();
 
 document.querySelector('#add-item-button').addEventListener('click', addItem);
 
