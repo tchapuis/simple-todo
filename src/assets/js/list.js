b5c8faea63bc02af72192f4d35fdad7faeec0e32
Sixtinebs <56111123+Sixtinebs@users.noreply.github.com>
2021-11-08 15:05:23 +0100

    Check status for new element (#9)
    * Check status for new element
    
    * eslint fix probleme is ok
    
    * Check item is works for already present elements
    
    * correction eslint
    
    * toggle class 'line-through'
    
    * correct eslint
    
    Co-authored-by: Thomas Chapuis <chapuis.t@gmail.com>

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -8,6 +8,19 @@ const generateItem = (id, content) => {
   return newListElement;
 };
 
+const checkItem = (e) => {
+  const { itemId } = e.currentTarget.dataset;
+  document.querySelector(`#item-${itemId} > label`).classList.toggle('line-through');
+};
+
+const clickCheckbox = () => {
+  document.querySelectorAll('.mark-item').forEach((checkbox) => {
+    checkbox.addEventListener('click', checkItem);
+  });
+};
+
+clickCheckbox();
+
 window.onload = () => {
   let tasks = '';
   if (localStorage.getItem('tasks')) {
@@ -46,19 +59,12 @@ const addItem = () => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 
   itemsListEl.append(generateItem(listElementsCount + 1, inputValue));
+
+  clickCheckbox();
   deleteI();
   newItem();
-};
-
-const checkItem = (e) => {
-  const { itemId } = e.currentTarget.dataset;
-  document.querySelector(`#item-${itemId} > label`).classList.add('line-through');
 };
 
 deleteI();
 
 document.querySelector('#add-item-button').addEventListener('click', addItem);
-
-document.querySelectorAll('.mark-item').forEach((checkbox) => {
-  checkbox.addEventListener('click', checkItem);
-});
