3ba305a9599f84af5d71a93fb342b1f632002471
Romain TEXIER <46653730+Liaxum@users.noreply.github.com>
2021-11-08 12:10:11 +0100

    Save the tasks on localstorage and load them at the start (#10)
    * Save the tasks on localstorage and load them at the start
    
    * Arrange the code style
    
    * Arrange the code style
    
    * Change split string into a JSON array
    
    * Arrange code style

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -2,17 +2,32 @@ const generateItem = (id, content) => {
   const newListElement = document.createElement('li');
   newListElement.id = `item-${id}`;
   newListElement.innerHTML = `<input type="checkbox" class="mark-item" id="mark-item-${id}" data-item-id="${id}">
-        <label for="mark-item-${id}">${content}</label>
-        <button id="delete-item-${id}">Delete</button>`;
-
+  <label for="mark-item-${id}">${content}</label>
+  <button id="delete-item-${id}">Delete</button>`;
   return newListElement;
+};
+
+window.onload = () => {
+  let tasks = '';
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    const itemsListEl = document.querySelector('#items-list');
+    const listElementsCount = itemsListEl.childElementCount;
+    tasks.forEach((el) => (el ? itemsListEl.append(generateItem(listElementsCount + 1, el.name)) : ''));
+  }
 };
 
 const addItem = () => {
   const itemsListEl = document.querySelector('#items-list');
   const inputValue = document.querySelector('#add-item-input').value;
   const listElementsCount = itemsListEl.childElementCount;
-
+  // Add save the item to the localstorage
+  let tasks = [];
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push({ name: inputValue });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
   itemsListEl.append(generateItem(listElementsCount + 1, inputValue));
 };
 
